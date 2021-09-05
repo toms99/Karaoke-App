65735f75dc07bb32029661240cf609c6623409a1
JUARSO <juan09fut.js@gmail.com>
2021-09-04 18:55:43 -0600

    Login

--- KaraokeFrontend/src/app/app-routing.module.ts
+++ KaraokeFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponentComponent} from "./LoginComponents/login-component/login-component.component";
 
-const routes: Routes = [];
+
+/**
+ * Se utiliza para moverse en las diferentes rutas del proyecto
+ */
+export let rutas: Routes;
+rutas = [ {path: '', component: LoginComponentComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(rutas)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
