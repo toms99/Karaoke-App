3b3609b6a687cf9798fdd338a77a3a8090adba75
Tomás <tomas.seguram@gmail.com>
2021-09-11 13:06:25 -0600

    karaoke streaming tab UI ready

--- KaraokeFrontend/src/app/stream/stream.component.ts
+++ KaraokeFrontend/src/app/stream/stream.component.ts
@@ -1,41 +1,69 @@
-import { Element } from '@angular/compiler';
-import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
-
-declare var anime: any;              // declare like this
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import song from "./summer.json";
 
 @Component({
   selector: 'app-stream',
   templateUrl: './stream.component.html',
   styleUrls: ['./stream.component.scss']
 })
-export class StreamComponent implements AfterViewInit {
-  // ngOnInit(): void {
-  //   throw new Error('Method not implemented.');
-  // }
+export class StreamComponent implements OnInit {
 
-  ngAfterViewInit(): void {
-    // Wrap every letter in a span
-    const textWrapper = document.querySelector('.an-1')!;
-    textWrapper.innerHTML = textWrapper.textContent!.replace(/\S/g, "<span class='letter'>$&</span>");
+  public isPlaying = false;
+  public lyrics = "hi";
+  public nextLyrics = "bye";
+  audio = new Audio();
 
-    anime.timeline({ loop: true })
-      .add({
-        targets: '.an-1 .letter',
-        scale: [4, 1],
-        opacity: [0, 1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 950,
-        delay: (el: any, i: number) => 70 * i
-      }).add({
-        targets: '.an-1',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-      });
+  constructor(private router: Router){}
 
+  ngOnInit() {
+    let lyrics = song.Lyrics;
+    this.lyrics = lyrics[0].words;
+    this.nextLyrics = lyrics[1].words;
+  }
+
+  playAudio() {
+    if (!this.isPlaying) {
+      this.audio.src = "../../assets/input1.wav";
+      this.audio.load();
+      this.audio.play();
+      this.isPlaying = true;
+      this.Run();
+
+    } else {
+      this.audio.pause();
+      this.audio.currentTime = 0;
+      this.isPlaying = false;
+    }
 
   }
+
+  async Run() {
+    let currentPos = 0;
+    let lyrics = song.Lyrics;
+    while (this.isPlaying) {
+      console.log("Im in")
+      lyrics.find(song => {
+        if (song.second == currentPos && currentPos >1) {
+          this.lyrics = this.nextLyrics;
+          this.nextLyrics = song.words;
+        }
+      })
+      currentPos++;
+      await this.wait(1000);
+    }
+    console.log("Im complete")
+  }
+
+  wait(ms: number) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  CloseWindow() {
+    this.router.navigateByUrl("/Usuario");
+  }
+
 
 }
