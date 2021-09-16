8a57359c2cee5369e1cce4b7308eab0364855b93
JUARSO <juan09fut.js@gmail.com>
2021-09-15 19:58:10 -0600

    A

---
+++ KaraokeFrontend/src/app/services/canciones.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Cancion} from "../Clases/Cancion";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CancionesService {
+  URL = 'http://localhost:3000/songs'
+  constructor(private http: HttpClient) {
+
+  }
+
+  public obtenerListaCancionesPublicas(user: string): Observable<Cancion[]>{
+    return this.http.get<Cancion[]>(this.URL, {params: {user}});
+  }
+
+  public subirUnaCancion(cancion: Cancion):Observable<Object>{
+    return this.http.post(this.URL, cancion);
+  }
+
+  public obtenerCancionPorID(id: string){
+    return this.http.get(this.URL + '/' +id);
+  }
+  public editarCancion(id: string){
+    return this.http.put(this.URL + '/' + id);
+  }
+
+
+}
