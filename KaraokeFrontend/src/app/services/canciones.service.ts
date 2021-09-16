15703039fb46d1ae7e3537f835d954ff2b3ca9d0
Bratan <alfabrayan12@gmail.com>
2021-09-15 20:05:04 -0600

    CRUD Songs

--- KaraokeFrontend/src/app/services/canciones.service.ts
+++ KaraokeFrontend/src/app/services/canciones.service.ts
@@ -8,24 +8,23 @@ import {Observable} from "rxjs";
 })
 export class CancionesService {
   URL = 'http://localhost:3000/songs'
-  constructor(private http: HttpClient) {
-
-  }
-
+  constructor(private http: HttpClient) {  }
   public obtenerListaCancionesPublicas(user: string): Observable<Cancion[]>{
     return this.http.get<Cancion[]>(this.URL, {params: {user}});
   }
-
   public subirUnaCancion(cancion: Cancion):Observable<Object>{
     return this.http.post(this.URL, cancion);
   }
-
   public obtenerCancionPorID(id: string){
     return this.http.get(this.URL + '/' +id);
   }
-  public editarCancion(id: string){
-    return this.http.put(this.URL + '/' + id);
+  public editarCancion(id: string, cancion: Cancion){
+    return this.http.put(this.URL + '/' + id, cancion);
   }
-
-
+  public eliminarCancion(id: string){
+    return this.http.delete(this.URL + '/' + id);
+  }
+  public eliminarCancionStorageOnly(id: string){
+    return this.http.delete(this.URL + '/' + id,  {params: {storageonly: true}});
+  }
 }
