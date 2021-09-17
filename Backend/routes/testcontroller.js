8e19e6ad857af09c03e581f6bdb66b34b484e403
Brayan <brayanleon25@hotmail.com>
2021-09-17 01:46:05 -0600

    all request to create user

--- Backend/routes/testcontroller.js
+++ Backend/routes/testcontroller.js
@@ -60,6 +60,211 @@ router.post('/key', function(req, res){
 });
 
 
+router.post('/admin', function(req, res){
+    let data =  {
+        grant_type:"password",
+        client_id:"nodejs-microservice",
+        client_secret:"0ab3d1dc-5d96-4fe8-9852-258a558be4cd",
+        username:"creator",
+        password:"creator1"
+      }
+    data=qs.stringify(data)
+      
+    const options = {
+    hostname: 'localhost',
+    port: 8080,
+    path: '/auth/realms/Karaoke-Realm/protocol/openid-connect/token',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    }
+    
+    const reqs =  https.request(options,  rest => {
+    console.log(`statusCode: ${res.statusCode}`)
+    let s=""
+    rest.on('data',  d => {
+        s =  JSON.parse(d.toString());
+            process.stdout.write(d);
+        })
+
+        rest.on('end', function () {
+            res.status(res.statusCode).jsonp(s)
+          });
+    })
+    
+    reqs.on('error', error => {
+        s=error
+        console.error(error)
+    })
+    reqs.write(data)
+    reqs.end()
+    
+
+});
+
+
+router.post('/create', function(req, res){
+    let data =  {
+        enabled:"true", username:"test_user"
+      }
+
+    data= JSON.stringify(data)
+    const options = {
+    hostname: 'localhost',
+    port: 8080,
+    path: '/auth/admin/realms/Karaoke-Realm/users',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization':'Bearer '+req.body.bearer
+    }
+    }
+    
+    const reqs =  https.request(options,  rest => {
+    console.log(`statusCode: ${res.statusCode}`)
+    let s=""
+    rest.on('data',  d => {
+        s =  JSON.parse(d.toString());
+            process.stdout.write(d);
+        })
+
+        rest.on('end', function () {
+            res.status(res.statusCode).jsonp(s)
+          });
+    })
+    
+    reqs.on('error', error => {
+        s=error
+        console.error(error)
+    })
+    reqs.write(data)
+    reqs.end()
+    
+
+});
+
+
+router.get('/get_user', function(req, res){
+    let data =  {
+        enabled:"true", username:"test_user"
+      }
+
+    data= JSON.stringify(data)
+    const options = {
+    hostname: 'localhost',
+    port: 8080,
+    path: '/auth/admin/realms/Karaoke-Realm/users?username=test_user',
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization':'Bearer '+req.body.bearer
+    }
+    }
+    
+    const reqs =  https.request(options,  rest => {
+    console.log(`statusCode: ${res.statusCode}`)
+    let s=""
+    rest.on('data',  d => {
+        s =  JSON.parse(d.toString());
+            process.stdout.write(d);
+        })
+
+        rest.on('end', function () {
+            res.status(res.statusCode).jsonp(s)
+          });
+    })
+    
+    reqs.on('error', error => {
+        s=error
+        console.error(error)
+    })
+    reqs.write(data)
+    reqs.end()
+    
+
+});
+
+router.get('/set_role', function(req, res){
+    let data =  [{
+        id:"fa1e9362-4dc4-4b65-ba98-b9e774b49516", name:"app-user"
+      }]
+
+    data= JSON.stringify(data)
+    const options = {
+    hostname: 'localhost',
+    port: 8080,
+    path: '/auth/admin/realms/Karaoke-Realm/users/'+ req.body.userid+'/role-mappings/realm',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization':'Bearer '+req.body.bearer
+    }
+    }
+    
+    const reqs =  https.request(options,  rest => {
+    console.log(`statusCode: ${res.statusCode}`)
+    let s=""
+    rest.on('data',  d => {
+        s =  JSON.parse(d.toString());
+            process.stdout.write(d);
+        })
+
+        rest.on('end', function () {
+            res.status(res.statusCode).jsonp(s)
+          });
+    })
+    
+    reqs.on('error', error => {
+        s=error
+        console.error(error)
+    })
+    reqs.write(data)
+    reqs.end()
+    
+
+});
+
+
+
+router.get('/set_pass', function(req, res){
+    let data =  { type: "password", temporary: false, value: req.body.password }
+
+    data= JSON.stringify(data)
+    const options = {
+    hostname: 'localhost',
+    port: 8080,
+    path: '/auth/admin/realms/Karaoke-Realm/users/'+ req.body.userid+'/reset-password',
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization':'Bearer '+req.body.bearer
+    }
+    }
+    
+    const reqs =  https.request(options,  rest => {
+    console.log(`statusCode: ${res.statusCode}`)
+    let s=""
+    rest.on('data',  d => {
+        s =  JSON.parse(d.toString());
+            process.stdout.write(d);
+        })
+
+        rest.on('end', function () {
+            res.status(res.statusCode).jsonp(s)
+          });
+    })
+    
+    reqs.on('error', error => {
+        s=error
+        console.error(error)
+    })
+    reqs.write(data)
+    reqs.end()
+});
+
+
+
 
 router.get('/admin', keycloak.protect('admin'), function(req, res){
     res.send("Hello Admin");
